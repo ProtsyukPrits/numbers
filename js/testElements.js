515c3ea8d3c53f672695d7efff055110b9a828fb
Protsiuk-Prits Dmytro <78557776+ProtsyukPrits@users.noreply.github.com>
2021-11-26 21:38:19 +0100

    button

--- js/testElements.js
+++ js/testElements.js
@@ -1,4 +1,58 @@
-const date = new Date()
-function btn() {
+let date = new Date()
+// function btn() {
+  
+//   document.getElementById('date').innerHTML = date
+// }
+
+const p = document.querySelector('button');
+
+p.onmouseover = logMouseOver;
+p.onmouseout = logMouseOut;
+
+
+
+
+
+function logMouseOver() {
   document.getElementById('date').innerHTML = date
+  document.getElementById('date').style.background = '#66beab'
+  document.getElementById('date').style.color = 'black'
+    
 }
+
+function logMouseOut() {
+  document.getElementById('date').innerHTML = 'Ours hour'
+  document.getElementById('date').style.background = 'white'
+  document.getElementById('date').style.color = 'black'
+    
+  
+}
+
+
+
+// const body = document.querySelector('body')
+// const clickTarget = document.getElementById('date')
+// const mouseOverTarget = document.getElementById('date')
+
+// let toggle = false;
+// function makeBackgroundYellow() {
+//     if (toggle) {
+//         body.style.backgroundColor = 'white';
+//     } else {
+//         body.style.backgroundColor = 'yellow';
+//     }
+
+//     toggle = !toggle;
+// }
+
+// clickTarget.addEventListener('click',
+//     makeBackgroundYellow,
+//     false
+// );
+
+// mouseOverTarget.addEventListener('mouseover', function () {
+//     clickTarget.removeEventListener('click',
+//         makeBackgroundYellow,
+//         false
+//     );
+// });
